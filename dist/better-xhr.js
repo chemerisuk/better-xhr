6e49589898f0b96a3a1319330e3ff9c83b62bf2d
Maksim Chemerisuk <chemerisuk@gmail.com>
2014-09-14 20:03:35 +0300

    version 0.2.0

---
+++ dist/better-xhr.js
@@ -0,0 +1,103 @@
+/**
+ * @file src/better-xhr.js
+ * @version 0.2.0 2014-09-14T20:03:35
+ * @overview Better abstraction for XMLHttpRequest
+ * @copyright Maksim Chemerisuk 2014
+ * @license MIT
+ * @see https://github.com/chemerisuk/better-xhr
+ */
+(function(global) {
+    var XHR = function(method, url, config) {
+        config = config || {};
+
+        var headers = config.headers || {},
+            charset = config.charset || "UTF-8",
+            data = config.data;
+
+        if (Object.prototype.toString.call(data) === "[object Object]") {
+            data = Object.keys(data).reduce(function(memo, key) {
+                var name = encodeURIComponent(key),
+                    value = data[key];
+
+                if (Array.isArray(value)) {
+                    value.forEach(function(value) {
+                        memo.push(name + "=" + encodeURIComponent(value));
+                    });
+                } else {
+                    memo.push(name + "=" + encodeURIComponent(value));
+                }
+
+                return memo;
+            }, []).join("&").replace(/%20/g, "+");
+        }
+
+        if (typeof data === "string") {
+            if (method === "get") {
+                url += (~url.indexOf("?") ? "&" : "?") + data;
+
+                data = null;
+            } else {
+                headers["Content-Type"] = "application/x-www-form-urlencoded";
+            }
+        }
+
+        if (Object.prototype.toString.call(config.json) === "[object Object]") {
+            data = JSON.stringify(config.json);
+
+            headers["Content-Type"] = "application/json; charset=" + charset;
+        }
+
+        return new Promise(function(resolve, reject) {
+            var xhr = new XMLHttpRequest();
+
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4) {
+                    var status = xhr.status;
+
+                    data = xhr.responseText;
+
+                    try {
+                        data = JSON.parse(data);
+                    } catch (err) {}
+
+                    if (status >= 200 && status < 300 || status === 304) {
+                        resolve(data);
+                    } else {
+                        reject(data);
+                    }
+                }
+            };
+
+            xhr.open(method.toUpperCase(), url, true);
+
+            if (!("X-Requested-With" in headers)) {
+                headers["X-Requested-With"] = "XMLHttpRequest";
+            }
+
+            Object.keys(headers).forEach(function(key) {
+                if (headers[key]) xhr.setRequestHeader(key, headers[key]);
+            });
+
+            xhr.send(data);
+        });
+    },
+    Promise;
+
+    XHR.get = function(url, config) {
+        return XHR("get", url, config);
+    };
+
+    XHR.post = function(url, config) {
+        return XHR("post", url, config);
+    };
+
+    if (typeof module !== "undefined" && module.exports) {
+        Promise = require("promise-polyfill");
+
+        module.exports = XHR;
+    } else {
+        Promise = global.Promise;
+
+        global.XHR = XHR;
+    }
+})(this);
